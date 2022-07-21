846b38893d1b00c6d4e4113000d6b415cbc3b407
Anastasia <derricksmac@Anyas-MacBook-Air.local>
2022-07-21 15:52:56 -0400

    added fighter screen

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import './App.css';
 import Fighter from './Fighter'
+import FighterScreen from './FighterScreen';
 
-
+import {useState} from 'react'
 
 function App() {
 
   const fighters = [
     {name:'Roy', color: 'blue'}, {name:'Ganondorf', color: 'red'},{ name:'Mario', color: 'lightblue'}, {name: 'Luigi', color: 'pink'}, {name:'Marth', color: 'blue'}, {name:'Link',color:'pink'}, {name:'Inkling', color:'purple'}, {name:'Samus', color:'lightgreen'},{name: 'Fox', color: 'lightgreen'}, {name:'Sheik', color:"blue"}, {name:'Zelda', color:'pink'}, {name:'Ken', color: 'red'}
   ]
+
+const [selectedFighter, setSelectedFighter] = useState()
 
   return (
     <div className="App">
@@ -16,11 +19,12 @@ function App() {
       {
         fighters.map((element, index) =>{
           return (
-           <Fighter fighter={element}/>
+           <Fighter fighter={element} setSelectedFighter={setSelectedFighter}/>
           );
         })
       }
     </div>
+  { selectedFighter && <FighterScreen/>}
     </div>
   );
 }
--- src/Fighter.js
+++ src/Fighter.js
@@ -1,14 +1,24 @@
-const Fighter = ({fighter}) => {
-    return ( <div style={{backgroundColor: fighter.color}}>
-       <img
-         className="fighter-img"
-         src={`https://www.smashbros.com/assets_v2/img/fighter/thumb_a/${fighter.name.toLowerCase()}.png`}
-         alt={fighter.name}
-        //  style={{backgroundColor: fighter.color}}
-         // key={}
-       />
-       <h5 className="fighter-name">{fighter.name}</h5>
-     </div>)
+const Fighter = ({fighter, setSelectedFighter}) => {
+
+  const handleClick = (element) => {
+  setSelectedFighter(element.name)
+  }
+
+    return (
+      <div
+        style={{ backgroundColor: fighter.color }}
+        onClick={() => {
+         setSelectedFighter(fighter.name)
+        }}
+      >
+        <img
+          className="fighter-img"
+          src={`https://www.smashbros.com/assets_v2/img/fighter/thumb_a/${fighter.name.toLowerCase()}.png`}
+          alt={fighter.name}
+        />
+        <h5 className="fighter-name">{fighter.name}</h5>
+      </div>
+    );
 }
 
 export default Fighter
